cc5841e83462e790937efef3a492a5f423268d87
uhlemann <uhlemann90@gmx.de>
2015-07-01 11:55:07 +0200

    Update SuvCar.java

--- Auto/SuvCar.java
+++ Auto/SuvCar.java
@@ -4,15 +4,12 @@ public class SuvCar extends Car
 	{
 		private int tank = 40;
 		
-		if (tank >= strecke)
+		for (int i = tank; i >= strecke; strecke -1)
 		{
-			while (strecke >=0)
-			{
-				position = position +1;
-				tank = tank -1;
-				strecke -1;
-		
-			}
+			position = position -1;
+			tank = tank -1;
+			
 		}
+
 	}
 }
